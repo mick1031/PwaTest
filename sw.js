996223c9ce8ef9c11b1ab2b1d5bda591a7bc8932
MIck1031 <qqq19871031@gmail.com>
2022-07-04 03:57:03 +0800

    first commit

---
+++ sw.js
@@ -0,0 +1,15 @@
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
+const OFFLINE_URL = 'offline.html';
+
+self.addEventListener('install', event => {
+   
+});
+
+self.addEventListener('activate', (event) => {
+    
+});
+
+self.addEventListener('fetch', (event) => {
+    
+});
